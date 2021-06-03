3e8fd647e4f01e015bf035d4ea982ee6b70e143a
Nacef1 <31379@ma-web.nl>
2021-06-03 12:13:00 +0200

    Werkende github.

---
+++ Mythe4/Assets/Script/SpawnNurikabe.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNurikabe : MonoBehaviour
+{
+    public Transform Spawnpoint;
+    public GameObject Prefab;
+    void OnTriggerEnter()
+    {
+        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+    }
+}
